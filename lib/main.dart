89e40a8cbf78e7345dc10edec4d094836f74abe8
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-06-09 15:40:48 +0700

    Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,133 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  BluetoothConnection? connection;
+  List<String> dataList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    FlutterBluetoothSerial.instance.onStateChanged().listen((state) {
+      if (state == BluetoothState.STATE_OFF) {
+        showSnackBar('Bluetooth is turned off');
+      } else if (state == BluetoothState.STATE_ON) {
+        showSnackBar('Bluetooth is turned on');
+      }
     });
+  }
+
+  void connectToDevice(BluetoothDevice device) async {
+    if (connection != null && connection!.isConnected) {
+      showSnackBar('Already connected to a device. Disconnect first.');
+      return;
+    }
+
+    try {
+      BluetoothConnection newConnection =
+          await BluetoothConnection.toAddress(device.address);
+      showSnackBar('Connected to the device');
+      setState(() {
+        connection = newConnection;
+      });
+      listenForData();
+    } catch (error) {
+      showSnackBar('Failed to connect to the device. Please try again.');
+    }
+  }
+
+  void listenForData() {
+    connection!.input?.listen((data) {
+      String decodedData = utf8.decode(data);
+      if (decodedData.contains(';')) {
+        List<String> values = decodedData.split(';');
+        if (values.length == 8) {
+          String crc = values[6];
+          print(crc);
+          setState(() {
+            dataList = values.sublist(1, 6);
+          });
+        }
+      }
+    }).onDone(() {
+      showSnackBar('Disconnected from the device');
+      setState(() {
+        connection = null;
+        dataList.clear();
+      });
+    });
+  }
+
+  void disconnectFromDevice() {
+    if (connection != null && connection!.isConnected) {
+      connection!.close();
+      showSnackBar('Disconnected from the device');
+      setState(() {
+        connection = null;
+        dataList.clear();
+      });
+    }
+  }
+
+  void showSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Bluetooth Demo'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              child: const Text('Connect to Device'),
+              onPressed: () {
+                showSnackBar('Searching for devices...');
+                FlutterBluetoothSerial.instance
+                    .openSettings()
+                    .then((value) => print("value"));
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: disconnectFromDevice,
+              child: const Text('Disconnect from Device'),
+            ),
+            const SizedBox(height: 20),
             const Text(
-              'You have pushed the button this many times:',
+              'Received Data:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 10),
+            Text(dataList.join('\n')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
